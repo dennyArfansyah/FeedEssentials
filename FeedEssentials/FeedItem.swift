08d88d258d9943f2bb3938cfa19973a872153b87
Denny  Arfansyah <denny.arfansyah@silentmode.my>
2025-08-15 16:25:00 +0700

    Fixing location of FeedItem model

---
+++ FeedEssentials/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FeedEssentials
+//
+//  Created by Denny  Arfansyah  on 15/08/2025.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let descruiption: String?
+    let locaton: String?
+    let imageURL: URL
+}
